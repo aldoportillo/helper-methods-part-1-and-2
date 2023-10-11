c58d4af7f4278780df06796647dcfd06c15e8783
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-11 14:48:54 +0000

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,19 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create", as: :movies
-  get "/movies/new" => "movies#new", as: :new_movie
+  post "/movies" => "movies#create", as: :movies ## => movies_path
+  get "/movies/new" => "movies#new", as: :new_movie  ## => new_movie_path
           
   # READ
-  get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show", as: :movie
+  get "/movies" => "movies#index" ## => movies_path
+  get "/movies/:id" => "movies#show", as: :movie ## => movie_path
   
   # UPDATE
-  patch "/movies/:id" => "movies#update" ##Method is implied since it is defined for the get
-  get "/movies/:id/edit" => "movies#edit", as: :edit_movie
+  patch "/movies/:id" => "movies#update" ##Method is implied since it is defined for the get ## => movies_path
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie ## => edit_movie_path
   
   # DELETE
-  delete "/movies/:id" => "movies#destroy" ##Method is implied since it is defined for the get
+  delete "/movies/:id" => "movies#destroy" ##Method is implied since it is defined for the get ## => movies_path
+
+  #------------------------------
 end
